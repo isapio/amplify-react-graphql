570c08160cf216a2d1e6025c978395a760a068ef
Eduardo Rosado <eduardoa1999@yahoo.com>
2024-04-20 00:53:50 -0400

    v3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import rosie from './rosie.svg'
+import daisy from './daisy.svg'
+
 
 function App() {
+  const [image, setImage] = useState(null);
+
+  const handleButtonClick = (imageName) => {
+    // Depending on the button clicked, set the appropriate image
+    if (imageName === 'Rosie') {
+      setImage(rosie);
+    } else if (imageName === 'Daisy') {
+      setImage(daisy);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
+    <div>
+      <text>Hello Christine, check out your cats!</text>
+      <div>
+      <button onClick={() => handleButtonClick('Rosie')}>Rosie</button>
+      <button onClick={() => handleButtonClick('Daisy')}>Daisy</button>
+      </div>
+      {/* Render the image if it's set */}
+      <div>{image && <img src={image} alt="Dog" style={{ marginTop: '10px' }}/>}</div>
     </div>
+    
   );
 }
 
